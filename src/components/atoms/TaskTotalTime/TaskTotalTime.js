cb6f22d2073c9d1e4f11a4048240f1e79a25b787
Mateusz Kuchta <mtkuchta@gmail.com>
2021-04-29 22:29:35 +0200

    total time counting improvement

--- src/components/atoms/TaskTotalTime/TaskTotalTime.js
+++ src/components/atoms/TaskTotalTime/TaskTotalTime.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const TaskTotalTime = ({ time }) => {
   const hours = Math.floor(time / 3600).toFixed(0);
-  const minutes = ((time % 3600) / 60).toFixed(0);
+  const minutes = Math.floor((time % 3600) / 60);
 
   return <StyledTime>{` Total work time: ${hours}h ${minutes}m`}</StyledTime>;
 };
